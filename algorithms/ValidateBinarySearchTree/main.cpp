04aea4950777e3a89ef1d419082d6414233d42de
Jack47 <Jack47.open>
2015-11-17 06:21:28 +0800

    add comment

--- algorithms/ValidateBinarySearchTree/main.cpp
+++ algorithms/ValidateBinarySearchTree/main.cpp
@@ -4,34 +4,39 @@ struct TreeNode {
     TreeNode* left;
     TreeNode* right;
     TreeNode(int v) : val(v), left(NULL), right(NULL) {
+
     }
 };
 class Solution {
 public:
     bool isValidBST(const TreeNode* root) {
-        if (root == NULL) {
+        if (root == NULL ||
+            (root->left == NULL && root->right == NULL)) {
             return true;
         }
-        int preValue = 0x80000000;
-        return isValidBST(root, &preValue);
+        int prevValue = 0x80000000;
+        bool hasPrevValue = false;
+        return isValidBST(root, hasPrevValue, &prevValue);
 
     }
-    bool isValidBST(const TreeNode* root, int* preValue) {
+    bool isValidBST(const TreeNode* root, bool hasPrevValue, int* preValue) {
         bool ret = true;
         if (root->left != NULL) {
-            ret = isValidBST(root->left, preValue);
-            if (*preValue > root->val || !ret) {
-                return false;
-            }
+            ret = isValidBST(root->left, hasPrevValue, preValue);
+            hasPrevValue = true;
+        }
+        if ( (hasPrevValue && *preValue >= root->val) || !ret) {
+            return false;
         }
         *preValue = root->val;
+        hasPrevValue = true;
         if (root->right != NULL) {
-            ret = isValidBST(root->right, preValue);
+            ret = isValidBST(root->right, hasPrevValue, preValue);
             if (!ret) {
                 return false;
             }
         }
+
         return true;
     }
-};
-      
+};       
