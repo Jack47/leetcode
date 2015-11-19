71c422685c21fd769c4a9197fce8a16ceb70ffd0
轩翊 <shengdong.csd@alibaba-inc.com>
2015-11-19 23:00:52 +0800

    update

--- algorithms/TwoSum/main.cpp
+++ algorithms/TwoSum/main.cpp
@@ -26,7 +26,7 @@ public:
     vector<int> twoSum(const vector<int>& nums, int target) {
         int numsSize = static_cast<int>(nums.size());
         vector<int> nums_index;
-        nums_index.reserve(numsSize);
+        nums_index.resize(numsSize);
         for (int i = 0; i < numsSize; i++) {
             nums_index[i] = i;
         }
@@ -58,13 +58,9 @@ public:
 
 
 int main() {
-    vector<int> nums;
-    nums.push_back(3);
-    nums.push_back(2);
-    nums.push_back(4);
-
+    vector<int> nums = {230,863,916,585,981,404,316,785,88,12,70,435,384,778,887,755,740,337,86,92,325,422,815,650,920,125,277,336,221,847,168,23,677,61,400,136,874,363,394,199,863,997,794,587,124,321,212,957,764,173,314,422,927,783,930,282,306,506,44,926,691,568,68,730,933,737,531,180,414,751,28,546,60,371,493,370,527,387,43,541,13,457,328,227,652,365,430,803,59,858,538,427,583,368,375,173,809,896,370,789};
     Solution solution;
-    int target = 6;
+    int target = 542;
     vector<int> answer = solution.twoSum(nums, target);
     for (int i = 0; i < static_cast<int>(answer.size()); i++) {
         printf("%d\t", answer[i]);
