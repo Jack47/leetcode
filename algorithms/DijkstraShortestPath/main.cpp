764d5b6bec55781479b0a50515e508d0d422e3b6
轩翊 <shengdong.csd@alibaba-inc.com>
2015-12-14 10:00:15 +0800

    fix bug

--- algorithms/DijkstraShortestPath/main.cpp
+++ algorithms/DijkstraShortestPath/main.cpp
@@ -4,6 +4,8 @@
 #include <stack>
 #include <vector>
 
+#include <stdio.h>
+
 using namespace std;
 struct Dist {
     int _len;
@@ -17,22 +19,29 @@ struct Dist {
 };
 void calculateSingleSourceShortestDistance(const vector<vector<int> >& adjacent_matrix, const int single_source_vertex, vector<int>* shortestDistance) {
     priority_queue<Dist> next_dist;
-    vector<bool> visited(false, adjacent_matrix.size());
+    vector<bool> visited;
 
     if (adjacent_matrix.size() == 0) {
         return;
     }
     assert(adjacent_matrix.size() == adjacent_matrix[0].size());
+    
+    visited.reserve(adjacent_matrix.size());
+    for (int i = 0; i < static_cast<int>(adjacent_matrix.size()); i++) {
+        visited.push_back(false);
+    }
+
+    shortestDistance->clear();
     shortestDistance->reserve(adjacent_matrix.size());
     visited[single_source_vertex] = true;
-    for (int i = 0; i < static_cast<int>(shortestDistance->size()); i++) {
-        (*shortestDistance)[i] = adjacent_matrix[single_source_vertex][i];
+    for (int i = 0; i < static_cast<int>(adjacent_matrix.size()); i++) {
+        shortestDistance->push_back(adjacent_matrix[single_source_vertex][i]);
         if ((*shortestDistance)[i] != INT_MAX && i != single_source_vertex) {
             next_dist.push(Dist((*shortestDistance)[i], i));
         }
     }
 
-    for (int visited_num = 1; visited_num <= adjacent_matrix.size(); visited_num++) {
+    for (int visited_num = 1; visited_num < static_cast<int>(adjacent_matrix.size()); visited_num++) {
         assert(next_dist.size() > 0);
         Dist dist = next_dist.top();
         while (visited[dist._vertex] == true) {
@@ -44,12 +53,13 @@ void calculateSingleSourceShortestDistance(const vector<vector<int> >& adjacent_
         visited[vertex] = true;
         (*shortestDistance)[vertex] = dist._len;
         int len = dist._len;
+        assert(len > 0 && len < INT_MAX);
         const vector<int>& adjacent = adjacent_matrix[vertex];
-        for (int i = 0; i < adjacent.size(); i++) {
-            if (i != vertex && (*shortestDistance)[i] > len + adjacent[i]) {
+        for (int i = 0; i < static_cast<int>(adjacent.size()); i++) {
+            if (i != vertex && visited[i] != true && adjacent[i] != INT_MAX && (*shortestDistance)[i] > len + adjacent[i]) {
                 (*shortestDistance)[i] = len + adjacent[i];
                 assert((*shortestDistance)[i] < INT_MAX);
-                next_dist.push(Dist(i, (*shortestDistance)[i]));
+                next_dist.push(Dist((*shortestDistance)[i], i));
             }
         }
     }
@@ -58,22 +68,25 @@ int main() {
     int test_case_number;
     int node_number;
     int edge_number;
-    scanf("%d\n", &test_case_number);
+    scanf("%d", &test_case_number);
     for (int i = 0; i < test_case_number; i++) {
-        scanf("%d %d\n", &node_number, &edge_number);
+        scanf("%d%d", &node_number, &edge_number);
         vector<vector<int> > adjacent_matrix;
         for (int j = 0; j < node_number; j++) {
-            vector<int> outer_len(INT_MAX, node_number);
+            vector<int> outer_len(node_number, INT_MAX);
             adjacent_matrix.push_back(outer_len);
         }
         int len;
         int node1, node2;
         for (int j = 0; j < edge_number; j++) {
-            scanf("%d %d %d\n", &node1, &node2, &len);
+            scanf("%d%d%d", &node1, &node2, &len);
             assert(adjacent_matrix[node1-1][node2-1] == adjacent_matrix[node2-1][node1-1]);
             int current = adjacent_matrix[node1-1][node2-1];
             current = current > len ? len : current;
             adjacent_matrix[node1-1][node2-1] = adjacent_matrix[node2-1][node1-1] = current;
+        }
+        for (int j = 0; j < node_number; j++) {
+            adjacent_matrix[j][j] = 0;
         }
         int single_source_vertex;
         scanf("%d", &single_source_vertex);
@@ -92,5 +105,6 @@ int main() {
                 }
             }
         }
+        printf("\n");
     }
 }
