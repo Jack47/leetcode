a0d05bf0efaf2b36db6df256f9a7f30dd81f761d
Jack47 <scsvip@gmail.com>
2016-03-05 08:33:45 +0800

    add ConstructBinaryTree

---
+++ algorithms/java/src/ConstructBinaryTree/ConstructBinaryTreeByInOrderAndPostOrder.java
@@ -0,0 +1,41 @@
+package ConstructBinaryTree;
+
+import TreeNode.TreeNode;
+
+public class ConstructBinaryTreeByInOrderAndPostOrder {
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        if (inorder == null || inorder.length == 0) return null;
+        return buildTree(inorder, 0, postorder, 0, inorder.length);
+    }
+
+    TreeNode buildTree(int[] inorder,
+                       int inorderStart,
+                       int[] postorder,
+                       int postorderStart,
+                       int length) {
+        assert length > 0 && length <= inorder.length;
+        assert inorderStart >= 0 && inorderStart < inorder.length;
+        assert postorderStart >= 0 && postorderStart < inorder.length;
+
+        TreeNode root = new TreeNode(postorder[postorderStart + length - 1]);
+        int rootIndex = find(inorder, inorderStart, inorderStart + length, root.val);
+        assert rootIndex >= inorderStart && rootIndex < inorderStart + length;
+        int leftLength = rootIndex - inorderStart;
+        int rightLength = length - leftLength - 1;
+        if (leftLength > 0) {
+            root.left = buildTree(inorder, inorderStart, postorder, postorderStart, leftLength);
+        }
+        if (rightLength > 0) {
+            root.right = buildTree(inorder, rootIndex+1, postorder, postorderStart+ leftLength, rightLength);
+        }
+        return root;
+    }
+
+    int find(int[] nums, int startIndex, int endIndex, int num) {
+        while (startIndex < endIndex && nums[startIndex] != num) {
+            startIndex++;
+        }
+        if (startIndex < endIndex) return startIndex;
+        else return -1;
+    }
+}
---
+++ algorithms/java/src/ConstructBinaryTree/ConstructBinaryTreeByPreOrderAndInOrder.java
@@ -0,0 +1,52 @@
+package ConstructBinaryTree;
+
+import TreeNode.TreeNode;
+
+/**
+ * https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
+ * Given preorder and inorder travseral of a binary tree, construct the tree.
+ * You may assume that duplicates do not exist in the tree.
+ */
+public class ConstructBinaryTreeByPreOrderAndInOrder {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        if (preorder == null || preorder.length == 0) return null;
+        return buildTree(preorder, 0, inorder, 0, inorder.length);
+    }
+
+    public TreeNode buildTree(int[] preorder,
+                              int preorderStart,
+                              int[] inorder,
+                              int inorderStart,
+                              int length) {
+        assert length > 0;
+        assert preorderStart >= 0 && preorderStart < preorder.length;
+        assert inorderStart >= 0 && inorderStart < preorder.length;
+
+        TreeNode rootNode = new TreeNode(preorder[preorderStart]);
+        int rootIndex = find(inorder, inorderStart, inorderStart + length,
+                rootNode.val);
+        assert rootIndex >= inorderStart && rootIndex < inorderStart + length : "rootIndex:" +
+                rootIndex;
+        int leftLength = rootIndex - inorderStart;
+        int rightLength = length - leftLength - 1;
+        if (leftLength > 0) {
+            rootNode.left = buildTree(preorder, preorderStart + 1, inorder, inorderStart,
+                    leftLength);
+        }
+        if (rightLength > 0) {
+            rootNode.right = buildTree(preorder, preorderStart + 1 + leftLength, inorder,
+                    rootIndex + 1, rightLength);
+        }
+        return rootNode;
+    }
+
+    public int find(int[] nums, int startIndex, int endIndex, int num) {
+        while (startIndex < endIndex && nums[startIndex] != num) {
+            startIndex++;
+        }
+        if (startIndex < endIndex && nums[startIndex] == num) {
+            return startIndex;
+        } else return -1;
+    }
+
+}
