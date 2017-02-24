9c89f4bd54cde73ef401c9775a1080506c97ec0d
Jack47 <scsvip@gmail.com>
2017-02-24 10:00:46 +0800

    implement ValidNumber

---
+++ algorithms/java/src/StringToInteger/ValidNumber.java
@@ -0,0 +1,71 @@
+package StringToInteger;
+
+// Validate if a given string is numeric
+// https://leetcode.com/problems/valid-number/?tab=Description
+public class ValidNumber {
+
+    private enum State {
+        Begin,
+        Sign,
+        Integer,
+        SkipIntegerPoint,
+        Point,
+        Fraction,
+        E,
+        E_And_Sign,
+        ScientificNotation,
+    }
+
+    public boolean isNumber(String s) {
+        State state = State.Begin;
+        s = s.trim();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '+' || s.charAt(i) == '-') {
+                if (state == State.Begin) {
+                    state = State.Sign;
+                } else if (state == State.E) {
+                    state = state.E_And_Sign;
+                }
+                else {
+                    return false;
+                }
+            } else if (s.charAt(i) >= '0' && s.charAt(i) <= '9') {
+                switch (state) {
+                    case Begin:
+                    case Sign:
+                    case Integer:
+                        state = State.Integer;
+                        break;
+                    case Point:
+                    case SkipIntegerPoint:
+                    case Fraction:
+                        state = State.Fraction;
+                        break;
+                    case E:
+                    case E_And_Sign:
+                    case ScientificNotation:
+                        state = State.ScientificNotation;
+                        break;
+                    default:
+                        System.out.printf("State unkown: %s", state);
+                        break;
+                }
+            } else if (s.charAt(i) == '.') {
+                if (state == State.Begin || state == State.Sign) {
+                    state = State.SkipIntegerPoint;
+                } else if (state == State.Integer) {
+                    state = State.Point;
+                } else {
+                    return false;
+                }
+            } else if ((s.charAt(i) == 'E' || s.charAt(i) == 'e') && (state == State.Integer
+                    || state == State.Fraction || state == State.Point)) {
+                state = State.E;
+            } else {
+                return false;
+            }
+        }
+        return state == State.Integer || state == State.Fraction
+                || state == State.ScientificNotation || state == State.Point;
+    }
+}
---
+++ algorithms/java/test/StringToInteger/ValidNumberTest.java
@@ -0,0 +1,74 @@
+package StringToInteger;
+
+import static junit.framework.TestCase.assertEquals;
+
+import org.junit.Test;
+
+public class ValidNumberTest {
+    @Test
+    public void test() {
+        ValidNumber validNumber = new ValidNumber();
+        boolean result = validNumber.isNumber("0");
+        assertEquals(true, result);
+
+        result = validNumber.isNumber("0.1");
+        assertEquals(true, result);
+
+        result = validNumber.isNumber(" 0.1 ");
+        assertEquals(true, result);
+
+        result = validNumber.isNumber("abc");
+        assertEquals(false, result);
+
+        result = validNumber.isNumber("1 a");
+        assertEquals(false, result);
+
+        result = validNumber.isNumber("2e10");
+        assertEquals(true, result);
+
+        result = validNumber.isNumber("2e10");
+        assertEquals(true, result);
+
+        result = validNumber.isNumber("2e-10");
+        assertEquals(true, result);
+
+        result = validNumber.isNumber("-2e-10");
+        assertEquals(true, result);
+
+        result = validNumber.isNumber("-2e--10");
+        assertEquals(false, result);
+
+        result = validNumber.isNumber("-2e-1-0");
+        assertEquals(false, result);
+
+        result = validNumber.isNumber("-200");
+        assertEquals(true, result);
+
+        result = validNumber.isNumber("e");
+        assertEquals(false, result);
+
+        result = validNumber.isNumber("e9");
+        assertEquals(false, result);
+
+        result = validNumber.isNumber("3.");
+        assertEquals(true, result);
+
+        result = validNumber.isNumber(".1");
+        assertEquals(true, result);
+
+        result = validNumber.isNumber(".");
+        assertEquals(false, result);
+
+        result = validNumber.isNumber("+.8");
+        assertEquals(true, result);
+
+        result = validNumber.isNumber("+1.e8");
+        assertEquals(true, result);
+
+        result = validNumber.isNumber("+46.e8");
+        assertEquals(true, result);
+
+        result = validNumber.isNumber("+.8e10");
+        assertEquals(true, result);
+    }
+}
